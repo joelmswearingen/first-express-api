1025c462f10b404123053aeddf8cd6052150a618
joelmswearingen <60490297+joelmswearingen@users.noreply.github.com>
2020-04-19 13:40:04 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.json({'message': 'Hello, web programmers!'})
+    res.json({'message': 'Hello, you beautiful web programmers!'})
 })
 
 module.exports = router
